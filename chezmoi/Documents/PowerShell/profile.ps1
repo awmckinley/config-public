dec46ac044ca61a00ca04f3e1e9aaf9896e9c731
Adam McKinley <adam@awmckinley.net>
2024-12-28 18:08:55 -0600

    feat: configure PowerShell

---
+++ chezmoi/Documents/PowerShell/profile.ps1
@@ -0,0 +1,20 @@
+# set environment
+$env:HOME = $HOME
+
+# use carapace
+$env:CARAPACE_BRIDGES = ''
+Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
+
+# use Starship
+starship init powershell --print-full-init | Out-String | Invoke-Expression
+
+# use Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# use vfox
+vfox activate pwsh | Out-String | Invoke-Expression
+
+# use zoxide
+zoxide init powershell | Out-String | Invoke-Expression
