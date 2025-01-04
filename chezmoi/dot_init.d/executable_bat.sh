ab23989b0d4fbca4b57fec8d878750897fac73e0
Adam McKinley <adam@awmckinley.net>
2025-01-04 12:43:36 -0600

    feat: init bat and bootdev

--- chezmoi/dot_init.d/executable_bat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v bat >/dev/null 2>&1; then
-	# rebuild themes
-	bat cache --build
-fi
--- chezmoi/dot_init.d/executable_bootdev.sh
+++
@@ -1,3 +0,0 @@
-if ! command -v bootdev >/dev/null 2>&1; then
-	go install github.com/bootdotdev/bootdev@latest
-fi
