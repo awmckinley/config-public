dc2314143086fa7b2f227093f8729d8814b5d0bf
Adam McKinley <adam@awmckinley.net>
2025-01-04 10:27:00 -0600

    feat: init burtncookie

--- chezmoi/dot_init.d/executable_burntcookie.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! test -x "${HOME}/.cargo/bin/burntcookie"; then
-	echo 'Install burntcookie'
-	cargo install --git https://github.com/horrorho/burnt-cookie.git
-	echo ''
-fi
