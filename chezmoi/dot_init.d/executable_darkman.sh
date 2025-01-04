b81e4400c12166c746f96ccf000f9de9c549f184
Adam McKinley <adam@awmckinley.net>
2025-01-04 10:11:46 -0600

    feat: init darkman and go

--- chezmoi/dot_init.d/executable_darkman.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v dconf >/dev/null 2>&1; then
-	dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-fi
--- chezmoi/dot_init.d/executable_go.sh
+++
@@ -1,2 +0,0 @@
-# install Go binaries to bin
-go env -w GOBIN="${HOME}/.local/bin"
