b32d448ccf50d0dade45bc7dae299eb22cb3501a
Adam McKinley <adam@awmckinley.net>
2025-01-09 09:21:25 -0600

    feat: configure mani

--- chezmoi/dot_profile.d/mani.zsh
+++ chezmoi/dot_profile.d/mani.zsh
@@ -1,4 +1,1 @@
-export MANI_CONFIG="${XDG_CONFIG_HOME}/mani/hosts/${HOST}.yaml"
-export MANI_USER_CONFIG="${XDG_CONFIG_HOME}/mani/config.yaml"
-
 alias mani-status-all='mani run status --all --ignore-non-existing | grep -v "^$" | grep -v "TASK"'
