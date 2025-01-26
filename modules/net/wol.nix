3e86599fdc82e924d9365f9babd729905663ab20
Adam McKinley <adam@awmckinley.net>
2025-01-26 16:50:02 -0600

    fix: use wakeonlan on Darwin

--- modules/net/wol.nix
+++ modules/net/wol.nix
@@ -1,5 +1,18 @@
-{ pkgs, ... }:
 {
+  isDarwin,
+  isLinux,
+  lib,
+  pkgs,
+  ...
+}:
+{ }
+// lib.optionalAttrs isDarwin {
+  homebrew.brews = [
+    # implements Wake On LAN
+    "wakeonlan"
+  ];
+}
+// lib.optionalAttrs isLinux {
   environment.systemPackages = with pkgs; [
     # implements Wake On LAN
     wol
